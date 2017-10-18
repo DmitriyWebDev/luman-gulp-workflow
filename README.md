2df0a27fde1cce3fc8db2d8ea10fc752198e03a6
luman-web <luman-web@yandex.ru>
2017-10-18 13:37:18 +0600

    upgraded moduleds, changed gulp-jade to pug-jade

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Luman Gulp-Workflow #
 
-To run the task runner you must to have the following software installed:
+To run the task runner the following software neet to be installed:
 - NodeJS
 - NPM
 - bower
@@ -28,5 +28,7 @@ To compile all of your html/css/js files into final compressed/minified version,
 ```
 gulp dist
 ```
+
+also, there's a possobility to send your compressed/minified files to remote server. To do this, you should to set up connection in /_gulpfile/settings.js in `module.exports.remoteConn` object.
 
 The source code of your project located in the ./src folder
--- gulpfile.js
+++ gulpfile.js
@@ -1,175 +1,147 @@
 'use strict';
 
-// Luman-Gulp-Workflow v2.0.0
+// Luman-Gulp-Workflow v3.0.0
 
 const gulp = require('gulp'),
-    settings = require('./_gulpfile/settings'),
-    errorHandlers = require('./_gulpfile/error-handlers'),
-    browserSync = require('browser-sync'),
-    plumber = require('gulp-plumber'),
-    jade = require('gulp-jade'),
-    sassToCss = require('gulp-sass'),
-    gcmq = require('gulp-group-css-media-queries'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifyCss = require('gulp-minify-css'),
-    babel = require('gulp-babel'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    prettify = require('gulp-html-prettify'),
-    wiredep = require('wiredep').stream,
-    gulpif = require('gulp-if'),
-    useref = require('gulp-useref'),
-    remove = require('remove'),
-    assetpaths = require('gulp-assetpaths'),
-    sftp = require('gulp-sftp'),
-    changed = require('gulp-changed'),
-    fs = require('fs');
+  settings = require('./_gulpfile/settings'),
+  errorHandlers = require('./_gulpfile/error-handlers'),
+  browserSync = require('browser-sync'),
+  plumber = require('gulp-plumber'),
+  pug = require('gulp-pug'),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  gcmq = require('gulp-group-css-media-queries'),
+  cleanCss = require('gulp-clean-css'),
+  babel = require('gulp-babel'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  wiredep = require('wiredep').stream,
+  gulpif = require('gulp-if'),
+  useref = require('gulp-useref'),
+  remove = require('remove'),
+  sftp = require('gulp-sftp'),
+  changed = require('gulp-changed'),
+  fs = require('fs');
 
-gulp.task('jadeTemplatesToHTML', () => {
-    return gulp.src('./src/jade/public/templates/**/*.jade')
-        .pipe(plumber({errorHandler: errorHandlers.onJadeError}))
-        .pipe(changed('./src/public/templates/', {extension: '.html'}))
-        .pipe(jade({
-            basedir: './src/jade/include/',
-            pretty: true
-        }))
-        .pipe(wiredep({
-            directory: './src/public/bower_components/',
-            ignorePath: /^(\/|\.+(?!\/[^\.]))+\.+\/public/
-        }))
-        .pipe(gulp.dest('./src/public/templates/'))
-        .pipe(browserSync.reload({stream:true}));
+gulp.task('liveRefresh', ['pugCompile'], () => {
+  return gulp.src('./src/public')
+    .pipe(browserSync.reload({stream:true}));
 });
 
-gulp.task('jadeIncludesToHTML', () => {
-    return gulp.src('./src/jade/public/templates/**/*.jade')
-        .pipe(plumber({errorHandler: errorHandlers.onJadeError}))
-        .pipe(jade({
-            basedir: './src/jade/include/',
-            pretty: true
-        }))
-        .pipe(wiredep({
-            directory: './src/public/bower_components/',
-            ignorePath: /^(\/|\.+(?!\/[^\.]))+\.+\/public/
-        }))
-        .pipe(gulp.dest('./src/public/templates/'))
-        .pipe(browserSync.reload({stream:true}));
+gulp.task('pugCompile', () => {
+  return gulp.src('./src/pug/public/templates/**/*.pug')
+    .pipe(plumber({errorHandler: errorHandlers.onPugError}))
+    .pipe(changed('./src/public/templates/', {extension: '.html'}))
+    .pipe(pug({
+      basedir: './src/pug/include',
+      pretty: true
+    }))
+    .pipe(wiredep({
+      directory: './src/public/bower_components/',
+      ignorePath: /^(\/|\.+(?!\/[^\.]))+\.+\/public/
+    }))
+    .pipe(gulp.dest('./src/public/'));
 });
 
-gulp.task('sassToCss', () => {
-    return gulp.src('./src/sass/*.scss')
-        .pipe(plumber({errorHandler: errorHandlers.onSassError}))
-        .pipe(sassToCss({
-            includePaths: [
-                'node_modules/susy/sass',
-                'node_modules/breakpoint-sass/stylesheets'
-            ],
-        }))
-        .pipe(gcmq())
-        .pipe(autoprefixer(settings.AUTOPREFIXER_BROWSERS))
-        .pipe(gulp.dest('./src/public/css/'))
-        .pipe(browserSync.reload({stream:true}));
+gulp.task('pug', ['pugCompile', 'liveRefresh']);
+
+gulp.task('sass', () => {
+  return gulp.src('./src/sass/*.scss')
+    .pipe(plumber({errorHandler: errorHandlers.onSassError}))
+    .pipe(sass({
+      includePaths: [
+        'node_modules/susy/sass',
+        'node_modules/breakpoint-sass/stylesheets'
+      ],
+    }))
+    .pipe(gcmq())
+    .pipe(autoprefixer(settings.AUTOPREFIXER_BROWSERS))
+    .pipe(gulp.dest('./src/public/css/'))
+    .pipe(browserSync.reload({stream:true}));
 });
 
-gulp.task('concatJS', () => {
-    return gulp.src('./src/js/*.js')
-        .pipe(plumber({errorHandler: errorHandlers.onBabelTranspileError}))
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('./src/public/js/'))
-        .pipe(browserSync.reload({stream:true}));
+gulp.task('js', () => {
+  return gulp.src('./src/js/**/*.js')
+    .pipe(plumber({errorHandler: errorHandlers.onBabelError}))
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./src/public/js/'))
+    .pipe(browserSync.reload({stream:true}));
 });
 
 // here we're making a final distribution
 // version of the project
 gulp.task('create_dist_folder', () => {
-    try {
-        remove.removeSync('./dist/');
-        console.log('dist folder has been recreated');
-    } catch (err) {
-        console.log('dist folder has been created.');
-    }
-    return gulp.src('./src/public/templates/**/*.html')
-        .pipe(prettify({indent_char: ' ', indent_size: 4}))
-        .pipe(useref({
-            searchPath: __dirname + '/src/public/',
-        }))
-        .pipe(gulpif('**/*.js', uglify()))
-        .pipe(gulpif('**/*.css', minifyCss()))
-        .pipe(gulp.dest('./dist/'));
+  try {
+    remove.removeSync('./dist/');
+    console.log('dist folder has been recreated');
+  } catch (err) {
+    console.log('dist folder has been created.');
+  }
+  return gulp.src('./src/public/**/*.html')
+    .pipe(useref({
+      searchPath: __dirname + '/src/public/',
+    }))
+    .pipe(gulpif('**/*.js', uglify()))
+    .pipe(gulpif('**/*.css', cleanCss({compatibility: 'ie8'})))
+    .pipe(gulp.dest('./dist/'));
 });
 
 let filesToMove = [
-    './src/public/imgs/**/*.*',
-    './src/public/fonts/**/*.*'
+  './src/public/imgs/**/*.*',
+  './src/public/fonts/**/*.*'
 ];
 
-gulp.task('change-paths', ['create_dist_folder'], () => {
-    return gulp.src(['./dist/**/*.html'])
-        .pipe(assetpaths({
-            newDomain: '/newDomain/',
-            oldDomain : '/',
-            docRoot : '/',
-            filetypes : ['jpg','jpeg','png','ico','gif','js','css'],
-            templates: true
-        }))
-        .pipe(gulp.dest('./dist/'));
-});
-
 gulp.task('move_dependent_folders', () => {
-    // the base option sets the relative root for the set of files,
-    // preserving the folder structure
-    return gulp.src(filesToMove, { base: './src/public/' })
-        .pipe(gulp.dest('./dist/'));
+  return gulp.src(filesToMove, { base: './src/public/' })
+    .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('dist',[
-    'create_dist_folder',
-    'move_dependent_folders',
-    'change-paths'
+  'create_dist_folder',
+  'move_dependent_folders'
 ]);
 
 // here we upload production version
 // of site on the server
 gulp.task('sftp', () => {
-    return gulp.src('./dist/**/*')
-    .pipe(sftp({
-        host: settings.remoteConn.host,
-        user: settings.remoteConn.user,
-        pass: settings.remoteConn.pass,
-        remotePath: settings.remoteConn.path
-    }));
+  return gulp.src('./dist/**/*')
+  .pipe(sftp({
+    host: settings.remoteConn.host,
+    user: settings.remoteConn.user,
+    pass: settings.remoteConn.pass,
+    remotePath: settings.remoteConn.path
+  }));
 });
 
 // here we declare a node server
 // and setting up several parametrs
-gulp.task('browserSync', () => {
-    browserSync({
-        server: {
-            baseDir: './src/public/'
-        },
-        port: 8080,
-        open: false,
-        notify: false
-    });
+gulp.task('serve', () => {
+  browserSync({
+    server: {
+      baseDir: './src/public/'
+    },
+    port: 8080,
+    open: false,
+    notify: false
+  });
 });
 
 // what files we're watching for
 gulp.task('watch', () => {
-    gulp.watch('./src/jade/public/**/*.jade', ['jadeTemplatesToHTML']);
-    gulp.watch('./src/jade/include/**/*.jade', ['jadeIncludesToHTML']);
-    gulp.watch('./src/sass/**/*.scss', ['sassToCss']);
-    gulp.watch('./src/js/*.js', ['concatJS']);
-    gulp.watch('./bower.json', ['jadeIncludesToHTML']);
+  gulp.watch('./src/pug/**/*.pug', ['pug']);
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+  gulp.watch('./src/js/*.js', ['js']);
+  gulp.watch('./bower.json', ['pug']);
 });
 
 // defalt task when we're using just 'gulp' command
 gulp.task('default',[
-    'browserSync',
-    'jadeIncludesToHTML',
-    'sassToCss',
-    'concatJS',
-    'watch'
+  'serve',
+  'pugCompile',
+  'sass',
+  'js',
+  'watch'
 ]);
